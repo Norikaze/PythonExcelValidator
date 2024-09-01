731dc14302f47081f9bcda755821d09812935d32
Rafal Salacinski <igenisus7@gmail.com>
2024-09-01 12:36:29 +0200

    refactor

--- main.py
+++ main.py
@@ -18,21 +18,21 @@ def compare_data_types(excel_file_name):
     column_data_type_excel = get_excel_data(excel_file_name)
     # print(column_data_type_excel.values())
     # print(column_data_type_json.values())
-
+    comparision_logs = []
     types_correct = True
     for (column_name, column_data), expected_data_type in zip(column_data_type_excel.items(), column_data_type_json.values()):
         # print(f"printing data_excel")
         # print(column)
         # print(f"printing data_json")
         # print(expected_data_type)
-        for cell_value in column_data:
-            if type(cell_value) == expected_data_type:
-                print("It's a match ✌")
-            else:
-                print(f"Not matching data type, expected {expected_data_type}, got value - {cell_value}, error in column - {column_name}")
+        # print(type(expected_data_type))
+        print(column_data)
+        for i, cell_value in enumerate(column_data, 2):
+            if type(cell_value) != expected_data_type:
+                comparision_logs.append(f"Not matching data type, expected {expected_data_type.__str__}, got value - {cell_value.__str__}, error in column - {column_name}, in row {i}")
                 types_correct = False
-
-    return types_correct
+    print(comparision_logs)
+    return comparision_logs
 
 def read_column_data_types():
     json_file = open("excel-definition.json", "r")
@@ -57,22 +57,22 @@ def read_column_data_types():
 
 
 if __name__ == "__main__":
-    excel_file_name = r"C:\Users\admin\PycharmProjects\ExcelStart\15isInRange.xlsx"
-    print("Getting column names...")
-    print(get_column_names())
-    print("Getting excel data..")
-    print(get_excel_data(excel_file_name))
-    print("Checking column number...")
-    print(check_column_number(excel_file_name))
-    print("Checking column names..")
-    print(check_column_names(excel_file_name))
-    print("Checking column order...")
-    print(check_column_order(excel_file_name))
+    # excel_file_name = r"C:\Users\admin\PycharmProjects\ExcelStart\15isInRange.xlsx"
+    # print("Getting column names...")
+    # print(get_column_names())
+    # print("Getting excel data..")
+    # print(get_excel_data(excel_file_name))
+    # print("Checking column number...")
+    # print(check_column_number(excel_file_name))
+    # print("Checking column names..")
+    # print(check_column_names(excel_file_name))
+    # print("Checking column order...")
+    # print(check_column_order(excel_file_name))
     print("comparing data types...")
     compare_data_types("15isInRange.xlsx")
-    print("Reading column data types...")
-    print(read_column_data_types())
-    print_excel_data(excel_file_name)
+    # print("Reading column data types...")
+    # print(read_column_data_types())
+    # print_excel_data(excel_file_name)
 
 
 
